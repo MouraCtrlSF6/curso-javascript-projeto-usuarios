ac66fafda811c209998b50f981bf301c7afcfaa2
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-16 19:10:18 -0300

    iniciando o segundo projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+const name = document.querySelector("#exampleInputName");
+const gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+const birth = document.querySelector("#exampleInputBirth");
+const country = document.querySelector("#exampleInputCountry");
+const email = document.querySelector("#exampleInputEmail");
+const password = document.querySelector("#exampleInputPassword");
+const photo = document.querySelector("#exampleInputFile");
+const adm = document.querySelector("#exampleInputAdmin"); 
+
